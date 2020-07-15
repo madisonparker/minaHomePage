1c33eb75d8c2a60b8c6c71619cc7d45021e1d1b3
Madison <madison@parkerworld.com>
2020-07-14 20:36:24 -0400

    Initial Commit

---
+++ minaHomePage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  minaHomePage
+//
+//  Created by Apple on 7/14/20.
+//  Copyright © 2020 Madison Parker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
